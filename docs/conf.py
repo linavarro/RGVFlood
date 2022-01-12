8969308592916840e0df44581f8a8c8bb44b7c93
Andrew Ernest <anernest@usa.net>
2022-01-12 15:20:11 -0600

    move rates slide template to _static

--- docs/conf.py
+++ docs/conf.py
@@ -68,7 +68,7 @@ numfig_format = {'figure': 'Figure: %s', 'table': 'Table: %s', 'code-block': 'Li
 # -- Options for HTML Slide output ---------------------------------------------------
 
 slide_theme = 'rates'
-slide_theme_path = ['.']
+slide_theme_path = ['_static']
 #slide_theme_options = {
 #    'presenters': [
 #        {
